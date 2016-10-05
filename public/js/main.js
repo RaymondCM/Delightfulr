6a73a5b793a418f6d3eb36405fbad7692f40a6b3
Raymond Kirk <ray.tunstill@live.co.uk>
2016-10-05 02:10:37 +0100

    Added Twitter/Box/Sentiment

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,7 @@ var AjaxCall = function () {
 
     _createClass(AjaxCall, [{
         key: 'getStatus',
-        value: function getStatus(phrase) {
+        value: function getStatus(phrase, callback) {
             var _this = this;
 
             $.ajax({
@@ -23,15 +23,16 @@ var AjaxCall = function () {
                 contentType: 'application/json',
                 url: '' + this.post + AjaxCall.removeAllSpecialCharacters(phrase),
                 success: function success(data) {
-                    return _this.onMessage(data);
+                    return _this.onMessage(data, callback);
                 }
             });
         }
     }, {
         key: 'onMessage',
-        value: function onMessage(data) {
+        value: function onMessage(data, callback) {
             this.data = data;
-            console.log(this.name + '=' + JSON.stringify(data));
+            //console.log(`${this.name}=${JSON.stringify(data)}`);
+            callback(data);
         }
     }], [{
         key: 'removeAllSpecialCharacters',
@@ -45,5 +46,72 @@ var AjaxCall = function () {
 "use strict";
 
 var sentiment = new AjaxCall("/getSentiment/");
+var tweets = new AjaxCall("/getTweets/");
 
-sentiment.getStatus("a");
+var currentUser = {};
+
+$(document).ready(function () {
+    //sentiment.getStatus("hello how are you beautiful", () => console.log("hello"));
+    //tweets.getStatus("_tabbby", (data) => console.log(rawStreamToTweets(data.value)));
+
+
+    $("#input").submit(function (event) {
+        console.log(document.getElementById("search").value);
+        $("#main").removeClass("vertical-center-row");
+        $(".sentiment_box").remove();
+        tweets.getStatus(document.getElementById("search").value, function (data) {
+            return rawStreamToTweets(data.value);
+        });
+        return false;
+    });
+});
+var count = 0;
+
+function createCube() {
+    var phrase = arguments.length <= 0 || arguments[0] === undefined ? "Tweets and links" : arguments[0];
+    var screenName = arguments.length <= 1 || arguments[1] === undefined ? "— Screen Name (@ID)" : arguments[1];
+    var sentiment = arguments[2];
+
+    var ID = "cube" + count++;
+    var step = 1;
+
+    $("#main").append("<div class=\"text-center col-xs-10 col-sm-10 col-md-6 col-lg-6 col-md-offset-3 col-sm-offset-1 col-xs-offset-1 sentiment_box\"><div class=\"cube-wrap example4\"><div id=\"" + ID + "\" class=\"cube\"><div class=\"cube-front\"><blockquote class=\"twitter-tweet\"><p id=\"content\">" + phrase + "</p><p id=\"screen-name\">\"" + screenName + "\"</p> </blockquote> </div><div class=\"cube-bottom\"></div></div> </div></div>");
+    $('#' + ID).addClass('step1').on('click', function () {
+        var old = step;
+        step = step > 1 ? 1 : 2;
+        $('#' + ID).addClass('step' + step).removeClass('step' + old);
+    });
+
+    //document.getElementById("main").appendChild(container);
+}
+
+function rawStreamToTweets(data) {
+    var statuses = [];
+    var user = "",
+        screenName = "";
+
+    if (data) {
+        data = JSON.parse(data);
+        if (data.length > 0) {
+            screenName = data[0].user.screen_name;
+            user = data[0].user.name;
+        }
+
+        for (var i = 0; i < data.length; ++i) {
+            statuses.push(data[i].text);
+        }
+    }
+    var final = [];
+    statuses.forEach(function (item, index, array) {
+        return sentiment.getStatus(item.replace(/[^a-zA-Z0-9+]/g, " "), function (data) {
+            final.push({
+                tweet: item,
+                sentiment: data.value
+            });
+            createCube(item, "— " + user + " (@" + screenName + ")");
+        });
+    });
+
+    console.log(final);
+    return final;
+}
--- routes.js
+++ routes.js
@@ -1,5 +1,29 @@
 var sentiment = require('sentiment');
 
+//Callback functions
+var error = function (err, response, body) {
+    console.log('ERROR [%s]', err);
+};
+var success = function (data) {
+    data = JSON.parse(data);
+    var statuses = [];
+    for (let i = 0; i < data.length; ++i)
+        statuses.push(data[i].text);
+    console.log('Data [%s]', statuses);
+};
+
+var Twitter = require('twitter-node-client').Twitter;
+
+//Get this data from your twitter apps dashboard
+var config = {
+    "consumerKey": "Km7a5nyO15ZKOaw0c0kuImHDg",
+    "consumerSecret": "M041Y92HaQqvOxogutRsyRf6UlnZSbyHZUcSJSGVsAdfwlDfwa",
+    "accessToken": "452467731-FasZkSfexiGTdXsj7QrcJQm2NHFoLXYiyHfr3fsl",
+    "accessTokenSecret": "MoGyukFzPhr1gOgvmzkjMwGXIyuhAUiT9UOeN8kcqH36Z"
+}
+
+var twitter = new Twitter(config);
+
 module.exports = (app, pir, gestures) => {
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname + '/index.html'));
@@ -17,6 +41,25 @@ module.exports = (app, pir, gestures) => {
         if (!req.body) return res.sendStatus(400)
         res.send({
             "value": sentiment(req.params.phrase)
+        });
+    });
+
+    app.get('/getTweets/:user', (req, res) => {
+        if (!req.body) return res.sendStatus(400)
+        var user = req.params.user;
+
+        twitter.getCustomApiCall('/users/lookup.json', {
+            screen_name: user
+        }, error, () => {
+            twitter.getUserTimeline({
+                screen_name: user,
+                count: '3'
+            }, error, (data) => {
+                //console.log("sending", data);
+                res.send({
+                    "value": data
+                });
+            });
         });
     });
 }
