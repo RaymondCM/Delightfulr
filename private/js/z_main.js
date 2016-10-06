aba7a4f27033cfb989d1d086e107088ab136a09e
Raymond Kirk <ray.tunstill@live.co.uk>
2016-10-06 22:56:40 +0100

    Added Caching, Fixed Colours and Status

--- private/js/z_main.js
+++ private/js/z_main.js
@@ -2,11 +2,27 @@ let twitterFeed = new AjaxCall("/getTweets/");
 let tweetCube = new TweetCube();
 
 $(document).ready(() => {
-    $("#input").submit(function (event) {
-        $("#main").removeClass("vertical-center-row");
+    $("#input").submit((event) => {
         tweetCube.removeAll();
         twitterFeed.getStatus(document.getElementById("search").value, parseResponse);
         return false;
+    });
+
+    $("#colourscale").on("click", (e) => {
+        if (tweetCube.clearCut) {
+            tweetCube.clearCut = false;
+            $("#colourscale").addClass('range');
+            $("#worst").css('background-color', "");
+            $("#average").css('background-color', "");
+            $("#best").css('background-color', "");
+
+        } else {
+            tweetCube.clearCut = true;
+            $("#colourscale").removeClass('range');
+            $("#worst").css('background-color', tweetCube.theme[0]);
+            $("#average").css('background-color', tweetCube.theme[1]);
+            $("#best").css('background-color', tweetCube.theme[2]);
+        }
     });
 });
 
